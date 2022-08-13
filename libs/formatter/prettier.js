29800cd92f99c1454bf41f079f7dbb62971c6d99
gialynguyen <gialynguyen@gmail.com>
2022-08-13 22:10:46 +0700

    refactor(libs/formatter): prettier warning

--- libs/formatter/prettier.js
+++ libs/formatter/prettier.js
@@ -3,6 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 const merge = require('deepmerge');
 const prettierSettings = require('./settings/prettier');
 
-const config = merge({}, prettierSettings);
+const config = merge({
+	rules: {
+		"prettier/prettier": 1
+	}
+}, prettierSettings);
 
 module.exports = config;
